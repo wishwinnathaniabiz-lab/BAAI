19bb433a1417930291a5bef47be6219117e658eb
Nathania <wishwinnathania.biz@gmail.com>
2025-09-24 16:27:58 +0800

    formula sum

--- Nathania_Sum.py
+++ Nathania_Sum.py
@@ -1,11 +1,15 @@
 #
 # Nathania, 2025/09/24
 # File: Nathania_Sum.py
-# Short description of the task
+# Add two numbers then print
 #
 
 # 1. Input
+X = 8
+Y = 7
 
 # 2. Process
+Sum = X + Y
 
 # 3. Output
+print(f'Result {Sum}')
